ae3213f4d10bfca8fb0a9822bae7fe31addde6ff
Tpro Oficial <77292734+tpro99oficial@users.noreply.github.com>
2022-01-13 16:23:58 -0500

    Create PC.py

---
+++ Converter Files/PC.py
@@ -0,0 +1,18 @@
+# Pepson OnePorox Converter File to Convert to PC
+
+# Under This Line Creates Variables of Every Key, Is Incompleted, You Can Contribute on It By Filling A Bug https://github.com/Pepson-Systems/OnePorox/issues in this Site
+
+Shift Key = Shift
+Double Point = :
+Comma And Point = ; 
+Hashtag = #
+Space Key = Space
+A Letter = A
+B Letter = B
+C Letter = C
+D Letter = D
+Right Click = RC
+Left Click = LC
+
+# This is Inclompleted, You Can Modify It Freely to Contribute the Source Code, Have Fun!
+# Tpro Oficial, Founder and CEO of Pepson
